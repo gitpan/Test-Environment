e0e17698f0d3415153053b58f0fe14487925042a
Jozef Kutej <jozef@kutej.net>
2007-10-15 20:12:03 +0000

    Import of JKUTEJ/Test-Environment-0.01 from CPAN.
    gitpan-cpan-distribution: Test-Environment
    gitpan-cpan-version:      0.01
    gitpan-cpan-path:         JKUTEJ/Test-Environment-0.01.tar.gz
    gitpan-cpan-author:       JKUTEJ
    gitpan-cpan-maturity:     released

---
+++ lib/Test/Environment.pm
@@ -0,0 +1,94 @@
+package Test::Environment;
+
+=head1 NAME
+
+Test::Environment - Base module for loading Test::Environment::Plugin::*
+
+=head1 SYNOPSIS
+
+	use Test::Environment qw{
+		PostgreSQL
+		Dump
+	};
+	
+	# now we have 'psql', 'dump_with_name', ... functions in current namespace.
+	# imported from Test::Environment::Plugin::PostreSQL and Test::Environment::Plugin::Dump
+
+	eq_or_diff(
+		[ psql(
+			'switches' => '--expanded',
+			'command'  => 'SELECT * FROM Table LEFT JOIN OtherTable USING (other_id) ORDER BY other_id;',
+		) ],
+		[ dump_with_name('test_01.dump') ],
+		'check db loading',
+	);
+
+=head1 DESCRIPTION
+
+This is the base module to load Test::Environment::Plugin::* modules.
+
+Also sets:
+
+	$ENV{'RUNNING_ENVIRONMENT'} = 'testing';
+
+The plugins will export their routines so you can use them in your tests.
+
+=cut
+
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+use Carp::Clan;
+use English '-no_match_vars';
+use File::Basename;
+
+BEGIN {
+	$ENV{'RUNNING_ENVIRONMENT'} = 'testing';
+}
+
+
+=head1 FUNCTIONS
+
+=head2 import()
+
+Will load choosen Test::Environment::Plugin::? plugins.
+
+=cut
+
+sub import {
+	my $package = shift;
+	my @args    = @_;
+
+	foreach my $plugin_name (@args) {
+		croak 'bad plugin name' if $plugin_name !~ m{^\w+(::\w+)*$}xms;
+		
+		my $plugin_module_name = 'Test::Environment::Plugin::'.$plugin_name; 
+		eval 'use '.$plugin_module_name.';';
+		if ($EVAL_ERROR) {
+			croak 'Failed to load "'.$plugin_module_name.'" - '.$EVAL_ERROR;
+		}
+	}
+}
+
+1;
+
+
+=head1 SEE ALSO
+
+Test::Environment::Plugin::* L<http://search.cpan.org/search?query=Test%3A%3AEnvironment%3A%3APlugin%3A%3A&mode=module>
+
+=head1 AUTHOR
+
+Jozef Kutej, E<lt>jozef@kutej.net<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (C) 2007 by Jozef Kutej
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself, either Perl version 5.8.8 or,
+at your option, any later version of Perl 5 you may have available.
+
+=cut
---
+++ lib/Test/Environment/Plugin/PostgreSQL.pm
@@ -0,0 +1,176 @@
+package Test::Environment::Plugin::PostgreSQL;
+
+=head1 NAME
+
+Test::Environment::Plugin::PostgreSQL - PostreSQL psql function for testing
+
+=head1 SYNOPSIS
+
+	use Test::Environment qw{
+		PostgreSQL
+	};
+	
+	# set database credentials
+	psql(
+		'database' => $config->{'db'}->{'database'},
+		'hostname' => $config->{'db'}->{'hostname'},
+		'username' => $config->{'db'}->{'username'},
+		'password' => $config->{'db'}->{'password'},
+	);
+	
+	# execute sql query
+	my @output = psql(
+		'switches' => '--expanded',
+		'command'  => 'SELECT * FROM Table',
+		# ..., see psql function description for more
+	)
+
+
+=head1 DESCRIPTION
+
+This plugin will export 'psql' function that can be used to execute PostreSQL psql command
+with lot of options for testing.
+
+=cut
+
+
+use strict;
+use warnings;
+
+our $VERSION = 0.01;
+
+use base qw{ Exporter };
+our @EXPORT = qw{
+	psql
+};
+our $debug = 0;
+
+use Carp::Clan;
+use String::ShellQuote;
+
+
+=head1 FUNCTIONS
+
+=head2 import
+
+All functions are exported 2 levels up. That is to the use Test::Environment caller.
+
+=cut
+
+sub import {
+	my $package = shift;
+
+	# export symbols two levels up - to the Test::Environment caller
+	__PACKAGE__->export_to_level(2, $package, @EXPORT);
+}
+
+
+=head2 psql()
+
+psql command executed easily. Here is the list of options that can be used.
+
+Option related to the connection to the database.
+
+	username
+	password
+	database
+	hostname
+	port
+
+By setting this PostreSQL %ENV variables will be set. So for psql command to the
+same databse you need to set them only once.
+
+The rest of the option related to the psql command.
+
+	command         - scalar or array ref of sql commands
+	switches        - scalar or array of additional psql switches
+	output_filename - the output will be written to this file (-o)
+	execution_path  - before executing psql change to that folder
+	stderr_redirect - will redirect stderr to stdout so that also error appears in the return value
+	debug           - turn on debug mode, it can be also done globaly by setting "$ENV{'IN_DEBUG_MODE'} = 1" 
+
+=cut
+
+sub psql {
+	my %arg = @_;
+	
+	$ENV{'PGUSER'}     = $arg{'username'} if exists $arg{'username'};
+	$ENV{'PGPASSWORD'} = $arg{'password'} if exists $arg{'password'};
+	$ENV{'PGDATABASE'} = $arg{'database'} if exists $arg{'database'};
+	$ENV{'PGHOST'}     = $arg{'hostname'} if exists $arg{'hostname'};
+	$ENV{'PGPORT'}     = $arg{'port'}     if exists $arg{'port'};	
+	
+	# function paramaters
+	my $command         = $arg{'command'};
+	my $output_filename = $arg{'output_filename'};
+	my $execution_path  = $arg{'execution_path'};
+	my $stderr_redirect = $arg{'stderr_redirect'};
+	
+	my @switches        = (ref $arg{'switches'} eq 'ARRAY' ? @{$arg{'switches'}} : $arg{'switches'} )
+		if exists $arg{'switches'};
+	
+	local $debug = 1 if ($arg{'debug'} or $ENV{'IN_DEBUG_MODE'});
+	
+	# if there is no command return nothink to do more
+	return if not defined $command;
+	
+	# if more commands then join them together
+	if (ref $command eq 'ARRAY') {
+		$command = join '; ', @{$command};
+	}
+	$command .= ';' if ($command !~ m{;\s*$}xms);
+	
+	
+	if (defined $output_filename) {
+		push(@switches, '-o', '"'.$output_filename.'"')
+	}
+	
+	# redirect stderr to stdout
+	open(STDERR, ">&STDOUT") if (defined $stderr_redirect);
+	
+	# chdir if needed
+	if (defined $execution_path) {
+		execute(    # done using execute so we can see it in the debug mode as cd command
+			'cd',
+			$execution_path,
+		);
+	}
+		
+	my @ret = execute(
+		'psql',
+		@switches,
+		'-c',
+		$command,
+	);
+	
+	return @ret if defined wantarray;
+	return join('', @ret);
+}
+
+
+=head2 execute
+
+Executes command as system and return output.
+
+In debug mode prints command to stderr.
+
+=cut
+
+sub execute {
+	my $cmd = shell_quote @_;
+	print STDERR '+ ', $cmd, "\n" if $debug;
+	my @ret = `$cmd`;
+}
+
+1;
+
+
+=head1 SEE ALSO
+
+Test::Environment L<http://search.cpan.org/perldoc?Test::Environment>
+
+=head1 AUTHOR
+
+Jozef Kutej - E<lt>jozef@kutej.netE<gt>
+
+=cut
